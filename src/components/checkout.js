adfed2e684e4c1ee5a560446727428fcd7a4c59d
praditya <praditya.rendi88@gmail.com>
2019-04-25 14:51:01 +0700

    Memperbaiki Structure Di Project

---
+++ src/components/checkout.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Jumbotron, Container, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+
+export default class Checkout extends Component {
+  componentDidMount() {
+    var x = sessionStorage.total;
+    document.getElementById("totalHarga").innerHTML = x;
+  }
+
+  render() {
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <div style={{ textAlign: "right" }}>
+              <h1 style={{ textAlign: "center" }} className="display-3">
+                Total yang harus di bayar:
+              </h1>
+              <p style={{ marginRight: "50px" }} id="totalHarga" />
+              <Button
+                style={{ marginRight: "1000px" }}
+                tag={Link}
+                to="/dashbor"
+                color="primary"
+              >
+                Kembali ke Dashbor
+              </Button>
+
+              <Button className="btn btn-warning">Pesan Sekarang</Button>
+            </div>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
--- src/containers/payment/payment.js
+++ src/containers/payment/payment.js
@@ -1,37 +1,12 @@
 import React, { Component } from "react";
-import { Jumbotron, Container, Button } from "reactstrap";
 import Header from "../../components/header";
-import { Link } from "react-router-dom";
+import Checkout from "../../components/checkout";
 export default class Payment extends Component {
-  componentDidMount() {
-    var x = sessionStorage.total;
-    document.getElementById("totalHarga").innerHTML = x;
-  }
-
   render() {
     return (
       <div>
         <Header name="payment" />
-        <Jumbotron fluid>
-          <Container fluid>
-            <div style={{ textAlign: "right" }}>
-              <h1 style={{ textAlign: "center" }} className="display-3">
-                Total yang harus di bayar:
-              </h1>
-              <p style={{ marginRight: "50px" }} id="totalHarga" />
-              <Button
-                style={{ marginRight: "1000px" }}
-                tag={Link}
-                to="/dashbor"
-                color="primary"
-              >
-                Kembali ke Dashbor
-              </Button>
-
-              <Button className="btn btn-warning">Pesan Sekarang</Button>
-            </div>
-          </Container>
-        </Jumbotron>
+        <Checkout />
       </div>
     );
   }
